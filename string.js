c22e2b2636c46651c30f5be22fe1258f585f7410
alexamaegazo <alexamaegazo5@gmail.com>
2024-09-14 19:31:11 +0800

    Initial commit with string.js

---
+++ string.js
@@ -0,0 +1,21 @@
+//Concatenate
+let favActorFirstName = "Choi";
+console.log("First name:", favActorFirstName);
+let favActorLastName = "Hyun-Wook";
+console.log("Last name:", favActorLastName);
+let fullname = favActorFirstName + ' ' + favActorLastName;
+console.log(fullname);
+
+//Uppercase
+let uppercase = 'Choi Hyun-Wook';
+console.log("Full name:", uppercase);
+
+//Lowercase
+let lowercase = 'BULAN ';
+console.log("Pet name:", lowercase);
+
+//Append
+let message = "My Favorite Actor is CHOI HYUN-WOOK" ;
+message = message += ' he is my favorite actor in twinkling watermelon';
+console.log(message);
+
